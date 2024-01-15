eeffeb95052219d98199f266c4e58492abe0975b
mcowan38 <156355679+mcowan38@users.noreply.github.com>
2024-01-15 11:20:53 -0500

    Add files via upload

---
+++ pid4profiles_3tasks.php
@@ -0,0 +1,1 @@
+{"F-1-1":"PID","F-1-1-1":"Independent","F-1-2-1":"Republican","F-1-3-1":"Republican","F-1-4-1":"Independent","F-2-1":"PID","F-2-1-1":"Independent","F-2-2-1":"Democrat","F-2-3-1":"Independent","F-2-4-1":"Independent","F-3-1":"PID","F-3-1-1":"Republican","F-3-2-1":"Republican","F-3-3-1":"Independent","F-3-4-1":"Independent"}
